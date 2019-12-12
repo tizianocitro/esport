ccbb6bbf856b86808b74c76f663077a549f79418
RaucciAngelo <raucciiangeloo@gmail.com>
2019-12-12 10:37:58 +0100

    Update TagliaBean

--- eSport/src/beans/TagliaBean.java
+++ eSport/src/beans/TagliaBean.java
@@ -1,5 +1,47 @@
 package beans;
 
 public class TagliaBean {
+	/*
+	 * Costruttore di default
+	 */
+	public TagliaBean() {
+		
+	}
+	
+	/*
+	 * @return prodotto
+	 */
+	public String getProdotto() {
+		return prodotto;
+	}
+	/*
+	 * @param prodotto
+	 */
+	public void setProdotto(String prodotto) {
+		this.prodotto = prodotto;
+	}
+	/*
+	 * @return misura
+	 */
+	public String getMisura() {
+		return misura;
+	}
+	/*
+	 * @param misura
+	 */
+	public void setMisura(String misura) {
+		this.misura = misura;
+	}
 
+
+
+
+	/*
+	 * Variabile d'istanza prodotto
+	 */
+	private String prodotto;
+	/*
+	 * Variabile d'istanza misura;
+	 */
+	private String misura;
 }
