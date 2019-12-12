9294b76d6f5c2d81a17d219ae2d1534b4320740d
RaucciAngelo <raucciiangeloo@gmail.com>
2019-12-12 11:25:58 +0100

    Update CarrelloItem

--- eSport/src/beans/CarrelloItem.java
+++ eSport/src/beans/CarrelloItem.java
@@ -1,26 +1,49 @@
 package beans;
 
 public class CarrelloItem {
+	
+	/*
+	 * Costruttore CarrelloItem
+	 */
 	public CarrelloItem() {
 		qt=1;
 	}
 	
+	/*
+	 * @return prodotto
+	 */
 	public ProdottoBean getProdotto() {
 		return prodotto;
 	}
 	
+	/*
+	 * @param prodotto
+	 */
 	public void setProdotto(ProdottoBean prodotto) {
 		this.prodotto=prodotto;
 	}
 	
+	/*
+	 * @return quantita
+	 */
 	public int getQt() {
 		return qt;
 	}
 	
+	/*
+	 * @param quantita
+	 */
 	public void setQt(int qt) {
 		this.qt=qt;
 	}
 
+	/*
+	 * Variabili d'istanza prodotto
+	 */
 	private ProdottoBean prodotto;
+	
+	/*
+	 * Variabili d'istanza quantita
+	 */
 	private int qt;
 }
